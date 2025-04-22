0f8dc12b41791d8ac37deae9cf20e3920ada4d90
LXaburtoM <lxaburto@uam.edu.ni>
2025-04-22 07:56:46 -0600

    ejercicio finalizado

---
+++ ejercisios/seman5.py
@@ -0,0 +1,10 @@
+calificacione = int(input("Ingrese la calificación de la evaluación: "))
+calificacione2 = int(input("Ingrese la calificación de la evaluación: "))
+calificacione3 = int(input("Ingrese la calificación de la evaluación: "))
+suma = calificacione + calificacione2 + calificacione3
+promedio = suma / 3
+print(f"calificacione: {calificacione:>3}\n"
+      f"calificacione2: {calificacione2:>3}\n"
+      f"calificacione3: {calificacione3:>3}\n"
+      f"promedio: {promedio:>3.0f}")
+
