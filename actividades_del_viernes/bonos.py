1d8c37f2fed404c0bbc705b9bfc90a616c183af5
LXaburtoM <lxaburto@uam.edu.ni>
2025-04-25 07:40:56 -0600

    bonos del trabajador

---
+++ actividades_del_viernes/bonos.py
@@ -0,0 +1,13 @@
+sueldo = float(input("Ingrese el sueldo del empleado: "))
+
+if sueldo > 3000:
+    bono = sueldo * 0.10
+    print(f"El bono es del 10%: {bono:.2f}")
+elif sueldo > 1500 and sueldo <= 3000:
+    bono = sueldo * 0.05
+    print(f"El bono es del 5%: {bono:.2f}")
+else:
+    bono = 0
+    print("No tiene bono.")
+
+print(f"Sueldo: {sueldo:.2f}, Bono: {bono:.2f}")
